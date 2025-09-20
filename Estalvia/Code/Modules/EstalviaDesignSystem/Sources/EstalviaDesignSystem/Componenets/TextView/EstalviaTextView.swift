6af380f72c652dddb605794f225f676bdebfa7b4
AlexApostol <apostol516@gmail.com>
2025-09-20 09:11:56 +0200

    add buttons

---
+++ Estalvia/Code/Modules/EstalviaDesignSystem/Sources/EstalviaDesignSystem/Componenets/TextView/EstalviaTextView.swift
@@ -0,0 +1,24 @@
+//
+//  EstalviaTextField.swift
+//  EstalviaDesignSystem
+//
+//  Created by Alex.personal on 14/9/25.
+//
+
+import SwiftUI
+
+public struct EstalviaTextView: View {
+    public var body: some View {
+        Text("EstalviaTextView")
+    }
+}
+
+
+
+
+#Preview {
+    EstalviaTextView()
+}
+
+
+

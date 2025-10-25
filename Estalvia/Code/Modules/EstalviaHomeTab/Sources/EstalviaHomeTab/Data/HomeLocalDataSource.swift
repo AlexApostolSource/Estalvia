3a9d19925461a096c6fe19e266e22dc7adc86bc7
AlexApostol <apostol516@gmail.com>
2025-10-25 09:24:41 +0200

    initila setup

---
+++ Estalvia/Code/Modules/EstalviaHomeTab/Sources/EstalviaHomeTab/Data/HomeLocalDataSource.swift
@@ -0,0 +1,10 @@
+//
+//  HomeLocalDataSource.swift
+//  EstalviaHomeTab
+//
+//  Created by Alex.personal on 25/10/25.
+//
+
+protocol HomeLocalDataSourceProtocol {
+	
+}
--- Estalvia/Code/Modules/EstalviaHomeTab/Sources/EstalviaHomeTab/EstalviaHomeTab.swift
+++ Estalvia/Code/Modules/EstalviaHomeTab/Sources/EstalviaHomeTab/EstalviaHomeTab.swift
@@ -1,2 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct EstalviaHomeTabView: View {
+	public var body: some View {
+		
+		Text("Hello, World!")
+	}
+}
+
+#Preview {
+	EstalviaHomeTabView()
+}

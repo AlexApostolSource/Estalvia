27d4772b5400f900bda72a8b750393c02330b1b0
AlexApostol <apostol516@gmail.com>
2025-11-02 18:23:59 +0100

    good enough swifltint

--- Estalvia/Code/Estalvia/Features/MainTabbar/MainTabBarController.swift
+++ Estalvia/Code/Estalvia/Features/MainTabbar/MainTabBarController.swift
@@ -24,6 +24,10 @@ final class MainTabBarController: UITabBarController, UITabBarControllerDelegate
 		delegate = self
 	}
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}

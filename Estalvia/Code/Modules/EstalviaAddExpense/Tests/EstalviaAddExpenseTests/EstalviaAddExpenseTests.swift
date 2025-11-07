c90fb09c22e18e29988ba75ef70c00fc90b5b1ac
AlexApostol <apostol516@gmail.com>
2025-11-07 16:52:32 +0100

    renaming

--- Estalvia/Code/Modules/EstalviaAddExpense/Tests/EstalviaAddExpenseTests/EstalviaAddExpenseTests.swift
+++ Estalvia/Code/Modules/EstalviaAddExpense/Tests/EstalviaAddExpenseTests/EstalviaAddExpenseTests.swift
@@ -1,5 +1,5 @@
+@testable import EstalviaAddExpense
 import Testing
-@testable import EstalviaPurchaseFlow
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.

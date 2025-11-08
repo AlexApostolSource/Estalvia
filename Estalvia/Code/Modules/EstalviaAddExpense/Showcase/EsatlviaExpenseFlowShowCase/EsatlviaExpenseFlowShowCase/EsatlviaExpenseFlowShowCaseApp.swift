71f3552a1201d095b0665a3c28114eeb318deda7
AlexApostol <apostol516@gmail.com>
2025-11-08 09:45:35 +0100

    add showcase

---
+++ Estalvia/Code/Modules/EstalviaAddExpense/Showcase/EsatlviaExpenseFlowShowCase/EsatlviaExpenseFlowShowCase/EsatlviaExpenseFlowShowCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsatlviaExpenseFlowShowCaseApp.swift
+//  EsatlviaExpenseFlowShowCase
+//
+//  Created by Alex.personal on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EsatlviaExpenseFlowShowCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Estalvia/Code/Modules/EstalviaAddExpense/Showcase/EsatlviaExpenseFlowShowCase/EsatlviaExpenseFlowShowCaseTests/EsatlviaExpenseFlowShowCaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  EsatlviaExpenseFlowShowCaseTests.swift
+//  EsatlviaExpenseFlowShowCaseTests
+//
+//  Created by Alex.personal on 8/11/25.
+//
+
+import Testing
+@testable import EsatlviaExpenseFlowShowCase
+
+struct EsatlviaExpenseFlowShowCaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
